e03a11d8bc66716bfdec3337eb3502585407c0db
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-03 10:16:19 +0200

    test

--- installer/source/kpayloads.c
+++ installer/source/kpayloads.c
@@ -37,6 +37,8 @@
   kernel_ptr = (uint8_t *)kernel_base;                                      \
   kernel_pmap_store = &kernel_ptr[K##x##_PMAP_STORE];                       \
   pmap_protect_p_patch = &kernel_ptr[K##x##_PMAP_PROTECT_P];                \
+  prison0_addr = (void **)&kernel_ptr[K##x##_PRISON_0];                     \
+  rootvnode_addr = (void **)&kernel_ptr[K##x##_ROOTVNODE];                  \  
   pmap_protect = (void *)(kernel_base + K##x##_PMAP_PROTECT);
 
 struct kpayload_payload_header {
@@ -288,11 +290,19 @@ static int kpayload_install_payload(struct thread *td, struct kpayload_install_p
   // Use "kmem" for all patches
   uint8_t *kmem;
 
+	struct ucred *cred;
+	struct filedesc *fd;
+
+	fd = td->td_proc->p_fd;
+	cred = td->td_proc->p_ucred;
+
   // Pointers to be assigned in build_kpayload macro
   void *kernel_pmap_store;
   uint8_t *pmap_protect_p_patch;
   uint8_t *payload_buffer;
-
+  void **prison0_addr;
+  void **rootvnode_addr;
+  
   void (*pmap_protect)(void *pmap, uint64_t sva, uint64_t eva, uint8_t pr);
 
   uint16_t fw_version = args->kpayload_payload_info->fw_version;
@@ -327,6 +337,169 @@ static int kpayload_install_payload(struct thread *td, struct kpayload_install_p
   if (!payload_data || payload_size < sizeof(payload_header) || payload_header->signature != 0x5041594C4F414458ull) { // `payloadx`
     return -1;
   }
+
+	cred->cr_uid = 0;
+	cred->cr_ruid = 0;
+	cred->cr_rgid = 0;
+	cred->cr_groups[0] = 0;
+
+	cred->cr_prison = *prison0_addr;
+	fd->fd_rdir = fd->fd_jdir = *rootvnode_addr;
+
+	// escalate ucred privs, needed for access to the filesystem ie* mounting & decrypting files
+	void *td_ucred = *(void **)(((char *)td) + 304); // p_ucred == td_ucred
+
+	// sceSblACMgrIsSystemUcred
+	uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
+	*sonyCred = 0xffffffffffffffff;
+
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *sceProcType = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceProcType = 0x3801000000000013; // Max access
+
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *sceRemotePlay = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceRemotePlay = 0x3800000000000019; // SceRemotePlay
+	
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *sceOSUPDATE = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceOSUPDATE = 0x3801000000000024; // sceOSUPDATE
+	
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *scevtr = (uint64_t *)(((char *)td_ucred) + 88);
+	*scevtr = 0x3800800000000002; // scevtr
+
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *NPXS20103 = (uint64_t *)(((char *)td_ucred) + 88);
+	*NPXS20103 = 0x3800000000000011; // NPXS20103
+	
+	// sceSblACMgrIsAllowedToUseUNK_PFS
+	uint64_t *UNK_PFS = (uint64_t *)(((char *)td_ucred) + 88);
+	*UNK_PFS = 0x380100000000000A; // UNK_PFS
+
+	// sceSblACMgrIsAllowedToUseUNK_ICC
+	uint64_t *UNK_ICC = (uint64_t *)(((char *)td_ucred) + 88);
+	*UNK_ICC = 0x3800800000000024; // UNK_ICC
+	
+	// sceSblACMgrIsAllowedToUsePupUpdate0
+	uint64_t *PupUpdate0 = (uint64_t *)(((char *)td_ucred) + 88);
+	*PupUpdate0 = 0x3800100000000001; // PupUpdate0
+	
+	// sceSblACMgrIsAllowedToUseSecureWebProcess
+	uint64_t *SecureWebProcess = (uint64_t *)(((char *)td_ucred) + 88);
+	*SecureWebProcess = 0x3800000010000003; // SecureWebProcess
+	
+	// sceSblACMgrIsAllowedToUseSceNKWebProcess
+	uint64_t *SceNKWebProcess = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceNKWebProcess = 0x3800000000010003; // SceNKWebProcess
+
+	// sceSblACMgrIsAllowedToUseSecureUIProcess
+	uint64_t *SecureUIProcess = (uint64_t *)(((char *)td_ucred) + 88);
+	*SecureUIProcess = 0x3800000000000033; // SecureUIProcess
+	
+	// sceSblACMgrIsAllowedToUseSceNKUIProcess
+	uint64_t *SceNKUIProcess = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceNKUIProcess = 0x380000000000003c; // SceNKUIProcess
+	
+	// sceSblACMgrIsAllowedToUseSceSysAvControl
+	uint64_t *SceSysAvControl = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceSysAvControl = 0x380000000000001; // SceSysAvControl
+
+	// sceSblACMgrIsAllowedToUseSceShellUI
+	uint64_t *SceShellUI = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceShellUI = 0x380000000000000f; // SceShellUI
+
+	// sceSblACMgrIsAllowedToUseSceShellCore
+	uint64_t *SceShellCore = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceShellCore = 0x3800000000000010; // SceShellCore
+
+	// sceSblACMgrIsAllowedToUseDecid
+	uint64_t *Decid = (uint64_t *)(((char *)td_ucred) + 88);
+	*Decid = 0x3800000000010003; // Decid
+
+	// sceSblACMgrGetDeviceSceVdecProxy
+	uint64_t *SceVdecProxy = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceVdecProxy = 0x3800000000000003; // SceVdecProxy
+
+	// sceSblACMgrGetDeviceSceVencProxy
+	uint64_t *SceVencProxy = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceVencProxy = 0x3800000000000004; // SceVencProxy
+	
+	// sceSblACMgrGetDeviceOrbisaudiod
+	uint64_t *Orbisaudiod = (uint64_t *)(((char *)td_ucred) + 88);
+	*Orbisaudiod = 0x3800000000000005; // Orbisaudiod
+	
+	// sceSblACMgrGetDeviceCoredump
+	uint64_t *Coredump = (uint64_t *)(((char *)td_ucred) + 88);
+	*Coredump = 0x3800000000000006; // Coredump
+
+	// sceSblACMgrGetDeviceOrbissetip
+	uint64_t *Orbissetip = (uint64_t *)(((char *)td_ucred) + 88);
+	*Orbissetip = 0x3800000000000008; // Orbissetip
+
+	// sceSblACMgrIsAllowedToUseGnmCompositor
+	uint64_t *GnmCompositor = (uint64_t *)(((char *)td_ucred) + 88);
+	*GnmCompositor = 0x3800000000000009; // GnmCompositor
+
+	// sceSblACMgrIsAllowedToUseSceGameLiveStreaming
+	uint64_t *SceGameLiveStreaming = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceGameLiveStreaming = 0x3800000000000012; // SceGameLiveStreaming
+	
+	// sceSblACMgrIsAllowedToUseSCE_SYS_SERVICES
+	uint64_t *SCE_SYS_SERVICES = (uint64_t *)(((char *)td_ucred) + 88);
+	*SCE_SYS_SERVICES = 0x3800000000010003; // SCE_SYS_SERVICES
+	
+	// sceSblACMgrIsAllowedToUseScePartyDaemon
+	uint64_t *ScePartyDaemon = (uint64_t *)(((char *)td_ucred) + 88);
+	*ScePartyDaemon = 0x3800000000000014; // ScePartyDaemon
+
+	// sceSblACMgrIsAllowedToUseSceAvCapture
+	uint64_t *SceAvCapture = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceAvCapture = 0x3800000000000015; // SceAvCapture
+	
+	// sceSblACMgrIsAllowedToUseSceVideoCoreServer
+	uint64_t *SceVideoCoreServer = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceVideoCoreServer = 0x3800000000000016; // SceVideoCoreServer	
+
+	// sceSblACMgrIsAllowedToUsemini_syscore
+	uint64_t *mini_syscore = (uint64_t *)(((char *)td_ucred) + 88);
+	*mini_syscore = 0x3800000000000022; // mini_syscore
+
+	// sceSblACMgrIsAllowedToUseSceCloudClientDaemon
+	uint64_t *SceCloudClientDaemon = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceCloudClientDaemon = 0x3800000000000028; // SceCloudClientDaemon
+	
+	// sceSblACMgrIsAllowedToUsefs_cleaner
+	uint64_t *fs_cleaner = (uint64_t *)(((char *)td_ucred) + 88);
+	*fs_cleaner = 0x380000000000001d; // fs_cleaner	
+
+	// sceSblACMgrIsAllowedToUseSceSocialScreenMgr
+	uint64_t *SceSocialScreenMgr = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceSocialScreenMgr = 0x3800000000000037; // SceSocialScreenMgr
+
+	// sceSblACMgrIsAllowedToUseSceSpZeroConf
+	uint64_t *SceSpZeroConf = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceSpZeroConf = 0x380000001000000E; // SceSpZeroConf
+
+	// sceSblACMgrIsAllowedToUseSceMusicCoreServer
+	uint64_t *SceMusicCoreServer = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceMusicCoreServer = 0x380000000000001a; // SceMusicCoreServer
+
+	// sceSblACMgrIsAllowedToUsesceSblACMgrHasUseHp3dPipeCapability
+	uint64_t *sceSblACMgrHasUseHp3dPipeCapability = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceSblACMgrHasUseHp3dPipeCapability = 0x3800000010000009; // sceSblACMgrHasUseHp3dPipeCapability
+
+	// sceSblACMgrIsAllowedToUsesceSblACMgrHasUseHp3dPipeCapability2
+	uint64_t *sceSblACMgrHasUseHp3dPipeCapability2 = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceSblACMgrHasUseHp3dPipeCapability2 = 0x380100000000002C; // sceSblACMgrHasUseHp3dPipeCapability2
+	
+	// sceSblACMgrIsAllowedToUseSceSysCore
+	uint64_t *SceSysCore = (uint64_t *)(((char *)td_ucred) + 88);
+	*SceSysCore = 0x3800000000000007; // SceSysCore	
+				
+	// sceSblACMgrHasSceProcessCapability
+	uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
+	*sceProcCap = 0xffffffffffffffff; // Sce Process
 
   // Disable write protection
   uint64_t cr0 = readCr0();
