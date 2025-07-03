96c5ce32653de8ffb4c77d81fee035ca8b046088
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-03 16:23:26 +0200

    Update main.c

--- installer/source/main.c
+++ installer/source/main.c
@@ -189,7 +189,7 @@ int _main(struct thread *td) {
   }
 
   //printf_notification("Welcome to HEN %s", VERSION);
-  printf_notification3("/user/data/icon0.png", "HEN-Vtx %s-" VERSION);
+  printf_notification3("/user/data/icon0.png", "HEN-Vtx-" VERSION);
   
   const bool kill_ui = true;
   const int sleep_sec = kill_ui ? 5 : 1;
