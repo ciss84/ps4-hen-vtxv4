0b2b6c999b770fd627b9b715e08c190291a17183
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-04 16:52:18 +0200

    test

--- installer/source/main.c
+++ installer/source/main.c
@@ -237,7 +237,7 @@ int _main(struct thread *td) {
      uint32_t CPU_Temp;
      uint32_t ret = sceKernelGetCpuTemperature(&CPU_Temp);
      printf_debug("returned %d\n%i", ret);
-     printf_notification("Temperature\n*Cpu: %d*C", CPU_Temp);
+     printf_notification3("/user/data/icon0.png", "*Cpu: %d*C", CPU_Temp);
      sceKernelSleep(100);
     }  
   }
