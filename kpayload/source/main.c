08a047bed104ac9a72eedf51aa58c3f77f8a69ba
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-05 11:02:03 +0200

    Update main.c

--- kpayload/source/main.c
+++ kpayload/source/main.c
@@ -17,7 +17,7 @@
 
 uint16_t fw_version PAYLOAD_BSS = 0;
 const struct kpayload_offsets *fw_offsets PAYLOAD_BSS = NULL;
-//struct configuration config PAYLOAD_BSS = {0};
+struct configuration config PAYLOAD_BSS = {0};
 
 int (*memcmp)(const void *ptr1, const void *ptr2, size_t num) PAYLOAD_BSS;
 int (*_sx_xlock)(struct sx *sx, int opts, const char *file, int line) PAYLOAD_BSS;
