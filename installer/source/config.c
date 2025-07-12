3a6e1815fb376280b34e798328d51b5c4d61315e
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-12 12:48:16 +0200

    test

--- installer/source/config.c
+++ installer/source/config.c
@@ -25,7 +25,7 @@
 #define MATCH(n) strcmp(name, n) == 0
 
 //static 
-void upload_ver(void) {
+static void upload_ver(void) {
   write_blob(BASE_PATH "/" VERSION_TXT, VERSION, sizeof(VERSION) - 1);
   write_blob(HDD_INI_PATH, hen_ini, hen_ini_len);
 }
