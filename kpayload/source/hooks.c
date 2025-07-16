104745336b052877508d186ad6b2609c4af609b1
Mugi <37186888+ciss84@users.noreply.github.com>
2025-07-16 11:08:02 +0200

    Update hooks.c

--- kpayload/source/hooks.c
+++ kpayload/source/hooks.c
@@ -299,7 +299,7 @@ PAYLOAD_CODE int sys_dynlib_load_prx_hook(struct thread *td, struct dynlib_load_
     struct dynlib_load_prx_args my_args = {};
     int handle = 0;
     if (isPartyDaemon) {
-      my_args.prx_path = PRX_SERVER_PATH;
+      //my_args.prx_path = PRX_SERVER_PATH;
     } else if (isShellUI) {
       my_args.prx_path = PRX_MONO_PATH;
     }
